9e25bebd7df3f461dbc98faf1775ad7115cdb46f
BasilRebresh <rebresh58gmail.com>
2022-06-24 16:10:54 +0300

    corrected naming for env links

--- src/components/PopUps/CreateNow/index.js
+++ src/components/PopUps/CreateNow/index.js
@@ -22,14 +22,14 @@ export default function CreateNow({ setShow }) {
     const userAgent = navigator.userAgent || navigator.vendor || window.opera
 
     if (/android/i.test(userAgent)) {
-      return process.env.LINK_TO_GOOGLEPLAY
+      return process.env.REACT_APP_LINK_TO_GOOGLEPLAY
     }
 
     if (/iPad|iPhone|iPod|Mac/.test(userAgent) && !window.MSStream) {
       console.log('process.env.LINK_TO_APPSTORE', process.env.LINK_TO_APPSTORE)
-      return process.env.LINK_TO_APPSTORE
+      return process.env.REACT_APP_LINK_TO_APPSTORE
     }
-    return process.env.LINK_TO_GOOGLEPLAY
+    return process.env.REACT_APP_LINK_TO_GOOGLEPLAY
   }
 
   return (
