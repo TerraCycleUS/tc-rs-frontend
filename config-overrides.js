5cc3e3d1c36534447f80bc093b8205761f9dca56
BasilRebresh <rebresh58gmail.com>
2022-08-08 13:17:53 +0300

    size optimization

--- config-overrides.js
+++ config-overrides.js
@@ -1,4 +1,11 @@
+const CompressionPlugin = require("compression-webpack-plugin");
 module.exports = function override(config, env) {
   //do stuff with the webpack config...
+  new CompressionPlugin({
+    test: /\.js(\?.*)?$/i,
+    filename: "[path][query]",
+    algorithm: "gzip",
+    deleteOriginalAssets: false,
+  })
   return config
 }
