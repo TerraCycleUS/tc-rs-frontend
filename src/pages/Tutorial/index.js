95c43724e8d74b6acb04ac58053d80f3bcb47802
BasilRebresh <rebresh58gmail.com>
2022-08-10 17:40:01 +0300

    images used in tutorial

--- src/pages/Tutorial/index.js
+++ src/pages/Tutorial/index.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import Page from '../../Layouts/Page'
 import { ReactComponent as RecycleSave } from '../../assets/icons/recycle-save.svg'
+import { ReactComponent as GetRewards } from '../../assets/images/get-rewards.svg'
+import { ReactComponent as DropThemOff } from '../../assets/images/drop-them-off.svg'
+import { ReactComponent as ScanYourItems } from '../../assets/images/scan-your-items.svg'
 import classes from './Tutorial.module.scss'
 
 export default function Tutorial() {
@@ -14,6 +17,7 @@ export default function Tutorial() {
             Scan accepted products and packaging to save them in your recycling
             bin.
           </p>
+          <ScanYourItems />
         </div>
         <div className={classes.step}>
           <h3 className={classes.stepTitle}>Drop them off</h3>
@@ -21,12 +25,14 @@ export default function Tutorial() {
             Find your local Monoprix and drop off your scanned products and
             packaging at the dedicated recycling point.
           </p>
+          <DropThemOff />
         </div>
         <div className={classes.step}>
           <h3 className={classes.stepTitle}>Get Rewards</h3>
           <p className={classes.stepText}>
             The more you recycle, the more rewards you will earn.
           </p>
+          <GetRewards />
         </div>
       </div>
     </Page>
