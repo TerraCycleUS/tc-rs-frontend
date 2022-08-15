7f71a9189766927c324247f6fd1c1c5511d34a15
BasilRebresh <rebresh58gmail.com>
2022-08-15 11:53:40 +0300

    added desktop banner for the home page

---
+++ src/utils/detectDesktop.js
@@ -0,0 +1,6 @@
+export default function detectDesktop() {
+  // if desktop return true
+  return !/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+    navigator.userAgent,
+  )
+}
