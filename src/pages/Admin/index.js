5faa3b1db654698b9cda30c606951d0524054bf9
BasilRebresh <rebresh58gmail.com>
2022-08-19 16:09:56 +0300

    admin panel start

--- src/pages/Admin/index.js
+++ src/pages/Admin/index.js
@@ -1,9 +1,15 @@
 import React from 'react'
-import { Admin } from 'react-admin'
+import { Admin, Resource } from 'react-admin'
 import jsonServerProvider from 'ra-data-json-server'
+import { ReactComponent as Avatar } from '../../assets/icons/avatar.svg'
 
 const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com')
-
+// console.log(dataProvider)
+// list={userList} edit={editUser} create={createUser}
 export default function AdminComponent() {
-  return <Admin dataProvider={dataProvider} />
+  return (
+    <Admin dataProvider={dataProvider}>
+      <Resource name="users" icon={Avatar} />
+    </Admin>
+  )
 }
