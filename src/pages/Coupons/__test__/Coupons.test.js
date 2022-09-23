c2ba8f991b6c4a754d87c61e3af574d627709230
BasilRebresh <rebresh58gmail.com>
2022-09-23 16:04:06 +0300

    test that coupons' page renders done

---
+++ src/pages/Coupons/__test__/Coupons.test.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { act, render } from '@testing-library/react'
+import Coupons from '../index'
+import store from '../../../store'
+import { setUser } from '../../../actions/user'
+import TestEnvironment from '../../../components/ForTestWriting/TestEnvironment'
+
+describe('Coupons', () => {
+  afterEach(() => {
+    act(() => {
+      store.dispatch(setUser(null))
+    })
+  })
+
+  test('it renders Coupons page', async () => {
+    store.dispatch(setUser({ user: 'mock' }))
+    const { debug } = render(
+      <TestEnvironment store={store}>
+        <Coupons />
+      </TestEnvironment>,
+    )
+    debug()
+  })
+})
