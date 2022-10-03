b1e368ada7fbfdce7dfb42689abea6846fdaa940
BasilRebresh <rebresh58gmail.com>
2022-10-03 16:58:12 +0300

    Retailer carousel and its responsivity finished

--- src/pages/SelectRetailer/index.js
+++ src/pages/SelectRetailer/index.js
@@ -1,9 +1,14 @@
-import React, { useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { Link } from 'react-router-dom'
 import { FormattedMessage } from 'react-intl'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import Button from '../../components/Button'
 import Page from '../../Layouts/Page'
 import RetailerMenu from '../../components/RetailerMenu'
+import detectIos from '../../utils/detectIos'
 import classes from './SelectRetailer.module.scss'
+// eslint-disable-next-line import/no-unresolved
+import 'swiper/scss'
 
 import MakeupSkincareIcon from '../../assets/icons/makeup-&-skincare.svg'
 import OralCareIcon from '../../assets/icons/oral-care.svg'
@@ -11,7 +16,7 @@ import GroomingIcon from '../../assets/icons/grooming.svg'
 import HairCareIcon from '../../assets/icons/hair-care.svg'
 import DeodorantsIcon from '../../assets/icons/deoderants.svg'
 import ShowerBathSoapIcon from '../../assets/icons/shower-bath-soap.svg'
-import Button from '../../components/Button'
+import getWindowSize from '../../utils/getWindowSize'
 
 const mockRetailers = [
   {
@@ -51,9 +56,50 @@ const mockRetailers = [
 export default function SelectRetailer() {
   const [activeRetailer, setActiveRetailer] = useState(-1)
   const [retailers] = useState(mockRetailers)
+  const [isIos] = useState(detectIos())
+  const swiperRef = useRef(null)
+  const [windowWidth, setWindowWidth] = useState(getWindowSize().innerWidth)
+  const [slidesShown, setSlidesShown] = useState(1.1)
+  const [spaceBetween, setSpaceBetween] = useState(7)
+
+  useEffect(() => {
+    if (swiperRef) {
+      swiperRef.current?.swiper.slideTo(activeRetailer)
+    }
+
+    if (windowWidth > 1700) {
+      setSlidesShown(4)
+      setSpaceBetween(40)
+    } else if (windowWidth > 1300) {
+      setSlidesShown(3)
+      setSpaceBetween(40)
+    } else if (windowWidth > 991) {
+      setSlidesShown(2)
+      setSpaceBetween(30)
+    } else if (windowWidth > 750) {
+      setSlidesShown(1.8)
+      setSpaceBetween(20)
+    } else if (windowWidth > 560) {
+      setSlidesShown(1.5)
+      setSpaceBetween(15)
+    } else {
+      setSlidesShown(1.1)
+      setSpaceBetween(7)
+    }
+
+    function handleWindowResize() {
+      setWindowWidth(getWindowSize().innerWidth)
+    }
+
+    window.addEventListener('resize', handleWindowResize)
+
+    return () => {
+      window.removeEventListener('resize', handleWindowResize)
+    }
+  }, [activeRetailer, windowWidth])
 
   return (
-    <Page backgroundGrey className="with-animation">
+    <Page width100 noSidePadding backgroundGrey className="with-animation">
       <RetailerMenu
         retailers={retailers.map((retailer) => ({
           id: retailer.id,
@@ -61,10 +107,19 @@ export default function SelectRetailer() {
         }))}
         setActiveRetailer={setActiveRetailer}
         activeRetailer={activeRetailer}
+        centeredSlides
       />
-      <div className={classes.carouselContainer}>
+      <Swiper
+        spaceBetween={spaceBetween}
+        onSlideChange={(swiper) => setActiveRetailer(swiper.activeIndex)}
+        cssMode={isIos}
+        className={classes.carouselContainer}
+        centeredSlides
+        slidesPerView={slidesShown}
+        ref={swiperRef}
+      >
         {retailers.map(({ id, name, iconUrl, backGroundImUrl, text }) => (
-          <div key={id} className={classes.carouselItem}>
+          <SwiperSlide key={id} className={classes.carouselItem}>
             <div className={classes.brandContainer}>
               <p className={classes.brandName}>{name}</p>
               <img
@@ -94,9 +149,9 @@ export default function SelectRetailer() {
                 />
               </Button>
             </Link>
-          </div>
+          </SwiperSlide>
         ))}
-      </div>
+      </Swiper>
     </Page>
   )
 }
