0444726c2a38901078d81cbd92cae389c6e26f85
BasilRebresh <rebresh58gmail.com>
2022-10-24 15:03:24 +0300

    data mappers for updates split in several files for better readability

--- src/components/ForAdminPanel/DataMappers/Update/formatForUpdate.js
+++ src/components/ForAdminPanel/DataMappers/Update/formatForUpdate.js
@@ -1,4 +1,5 @@
-import http from '../../../../utils/http'
+import forUpdateUser from '../../User/UserMapper/forUpdateUser'
+import forUpdateCoupon from '../../Coupon/CouponMapper/forUpdateCoupon'
 
 export default function formatForUpdate(resource, data, language, token) {
   switch (resource) {
@@ -9,79 +10,4 @@ export default function formatForUpdate(resource, data, language, token) {
     default:
       return data
   }
-}
-
-function forUpdateUser(user) {
-  const userFields = user
-  if (userFields.retailerId === '') userFields.retailerId = null
-  delete userFields.status
-  delete userFields.id
-  delete userFields.role
-  return userFields
-}
-
-async function forUpdateCoupon(coupon, language, token) {
-  if (coupon.brandLogo?.rawFile || coupon.backgroundImage?.rawFile)
-    return couponUpdateFiles(coupon, language, token)
-  return formatCoupon(coupon, language)
-}
-
-function formatCoupon(coupon, language, brandUrl = null, backgroundUrl = null) {
-  const couponFields = coupon
-  couponFields.langId = language
-  if (brandUrl) {
-    couponFields.brandLogo = `${process.env.REACT_APP_SERVER_API_URL}/api/file/${brandUrl}`
-  }
-  if (backgroundUrl) {
-    couponFields.backgroundImage = `${process.env.REACT_APP_SERVER_API_URL}/api/file/${backgroundUrl}`
-  }
-  Object.keys(couponFields).forEach((key) => {
-    if (couponFields[key] === null) {
-      delete couponFields[key]
-    }
-  })
-  delete couponFields.status
-  delete couponFields.id
-  delete couponFields.createdAt
-  delete couponFields.updatedAt
-  delete couponFields.retailerId
-  return couponFields
-}
-
-async function couponUpdateFiles(coupon, language, token) {
-  let newBrandLogoUrl = null
-  let newBackgroundImageUrl = null
-  const sendFileConfig = {
-    headers: {
-      Authorization: `Bearer ${token}`,
-      'Content-Type': 'multipart/form-data',
-    },
-  }
-
-  const brandData = new FormData()
-  brandData.append('file', coupon.brandLogo.rawFile)
-  if (coupon.brandLogo.rawFile) {
-    newBrandLogoUrl = await http.post(
-      '/api/upload/product',
-      brandData,
-      sendFileConfig,
-    )
-  }
-
-  const backgroundData = new FormData()
-  backgroundData.append('file', coupon.backgroundImage.rawFile)
-  if (coupon.backgroundImage.rawFile) {
-    newBackgroundImageUrl = await http.post(
-      '/api/upload/product',
-      backgroundData,
-      sendFileConfig,
-    )
-  }
-
-  return formatCoupon(
-    coupon,
-    language,
-    newBrandLogoUrl?.data?.name,
-    newBackgroundImageUrl?.data?.name,
-  )
 }
