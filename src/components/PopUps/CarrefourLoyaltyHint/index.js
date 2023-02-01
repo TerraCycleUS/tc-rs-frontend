2250502d762727bbd7857c323629caeb8a146369
BasilRebresh <rebresh58gmail.com>
2023-02-01 16:29:30 +0200

    created CarrefourLoyaltyHint pop-up, started using it in SetCarrefourLoyaltyId and ScanLoyaltyCard

---
+++ src/components/PopUps/CarrefourLoyaltyHint/index.js
@@ -0,0 +1,74 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { FormattedMessage } from 'react-intl'
+import classNames from 'classnames'
+import classes from './CarrefourLoyaltyHint.module.scss'
+import popClasses from '../GenericPop/GenericPop.module.scss'
+import { ReactComponent as Xmark } from '../../../assets/icons/x-mark.svg'
+import CardPass from '../../../assets/images/card-pass.png'
+
+export default function CarrefourLoyaltyHint({ closePop }) {
+  return (
+    <div className={popClasses.popWrapper}>
+      <div
+        className={classNames(
+          popClasses.popContainer,
+          popClasses.max400,
+          classes.popContainer,
+        )}
+      >
+        <Xmark onClick={() => closePop()} className={popClasses.closeBtn} />
+        <h2
+          className={classNames(
+            'my-text-h2',
+            'my-color-textBlack',
+            classes.title,
+          )}
+        >
+          <FormattedMessage
+            id="carrefourLoyaltyHint:Title"
+            defaultMessage="Loyalty ID"
+          />
+        </h2>
+        <p
+          className={classNames(
+            'my-text my-color-textPrimary text-center',
+            classes.text,
+          )}
+        >
+          <FormattedMessage
+            id="carrefourLoyaltyHint:Description"
+            defaultMessage="You can find your Loyalty ID on your cards as highlighted below:"
+          />
+        </p>
+        <p className={classes.cardLabel}>
+          <FormattedMessage
+            id="carrefourLoyaltyHint:CarrefourCard"
+            defaultMessage="Carrefour Card"
+          />
+        </p>
+        <div className={classNames(classes.cardWrap, classes.carrefourCard)}>
+          <img
+            src={CardPass}
+            alt="carrefour card"
+            className={classes.cardImg}
+          />
+          <div className={classes.cardBorder} />
+        </div>
+        <p className={classes.cardLabel}>
+          <FormattedMessage
+            id="carrefourLoyaltyHint:PassCard"
+            defaultMessage="Pass Card"
+          />
+        </p>
+        <div className={classNames(classes.cardWrap, classes.passCard)}>
+          <img src={CardPass} alt="pass card" className={classes.cardImg} />
+          <div className={classes.cardBorder} />
+        </div>
+      </div>
+    </div>
+  )
+}
+CarrefourLoyaltyHint.propTypes = {
+  closePop: PropTypes.func,
+}
