78df00521a5c924ae11401b2867826694e0e2010
BasilRebresh <rebresh58gmail.com>
2023-02-24 14:49:02 +0200

    new test

---
+++ src/pages/ScanLoyalty/__test__/ScanLoyalty.test.js
@@ -0,0 +1,56 @@
+import React from 'react'
+import { render } from '@testing-library/react'
+import TestEnvironment from '../../../components/ForTestWriting/TestEnvironment'
+import store from '../../../store'
+import ScanLoyalty from '..'
+import { setUser } from '../../../actions/user'
+
+jest.mock('../../../utils/http')
+jest.mock('../../../utils/useApiCall', () => () => jest.fn(() => {}))
+
+describe('ScanLoyalty ', () => {
+  beforeEach(() => {
+    const mockGetUserMedia = jest.fn(
+      async () =>
+        new Promise((resolve) => {
+          resolve()
+        }),
+    )
+
+    global.navigator.mediaDevices = {
+      enumerateDevices: jest.fn(),
+    }
+
+    Object.defineProperty(global.navigator, 'mediaDevices', {
+      value: {
+        getUserMedia: mockGetUserMedia,
+      },
+    })
+
+    // canvas mock
+    HTMLCanvasElement.prototype.getContext = jest.fn(() => ({
+      fillRect: jest.fn(),
+      clearRect: jest.fn(),
+      beginPath: jest.fn(),
+      moveTo: jest.fn(),
+      lineTo: jest.fn(),
+      stroke: jest.fn(),
+      arc: jest.fn(),
+      fill: jest.fn(),
+      drawImage: jest.fn(),
+      putImageData: jest.fn(),
+      setTransform: jest.fn(),
+      save: jest.fn(),
+      restore: jest.fn(),
+    }))
+  })
+
+  test('it renders ScanLoyalty page', async () => {
+    store.dispatch(setUser({ user: 'mock' }))
+    render(
+      <TestEnvironment store={store}>
+        <ScanLoyalty />
+      </TestEnvironment>,
+    )
+  })
+})
