e2c64ac17b023243f5998328c828806072bacfbe
BasilRebresh <rebresh58gmail.com>
2023-02-28 16:26:04 +0200

    sorting script improvement

--- locales/sortScript.js
+++ locales/sortScript.js
@@ -1,33 +1,40 @@
 const fs = require('fs');
 
-const data = fs.readFileSync('fr.json');
-const jsonData = JSON.parse(data);
-const dataArray = Object.entries(jsonData);
+function sortJson(readFile, writeFile = null) {
+  const data = fs.readFileSync(readFile);
+  const jsonData = JSON.parse(data);
+  const dataArray = Object.entries(jsonData);
 
-const arrayOfJson = []
-let enObj
-let frObj
-Object.entries(dataArray).forEach(( [,value]) => {
-  if (value[0] === 'en') {
-    enObj = {id: value[0], defaultValue: value[1]}
-  } else if (value[0] === 'fr') {
-    frObj = {id: value[0], defaultValue: value[1]}
-  } else arrayOfJson.push({id: value[0], defaultValue: value[1]})
-});
+  const arrayOfJson = []
+  let enObj
+  let frObj
+  Object.entries(dataArray).forEach(( [,value]) => {
+    if (value[0] === 'en') {
+      enObj = {id: value[0], defaultValue: value[1]}
+    } else if (value[0] === 'fr') {
+      frObj = {id: value[0], defaultValue: value[1]}
+    } else arrayOfJson.push({id: value[0], defaultValue: value[1]})
+  });
 
-const sorted = arrayOfJson.sort((a, b) => {
-  const keysA = a.id;
-  const keysB = b.id;
-  return keysA.localeCompare(keysB);
-});
+  const sorted = arrayOfJson.sort((a, b) => {
+    const keysA = a.id;
+    const keysB = b.id;
+    return keysA.localeCompare(keysB);
+  });
 
-sorted.unshift(enObj, frObj)
+  sorted.unshift(enObj, frObj)
 
 
-const newObj = {}
-sorted.forEach(entry => {
-  newObj[entry.id] = entry.defaultValue
-})
-fs.writeFileSync('fr-sorted.json', JSON.stringify(newObj, null, 1));
+  const newObj = {}
+  sorted.forEach(entry => {
+    newObj[entry.id] = entry.defaultValue
+  })
+  fs.writeFileSync('fr.json', JSON.stringify(newObj, null, 1));
+
+  fs.writeFileSync(writeFile || `sorted-${readFile}`, JSON.stringify(newObj, null, 1));
+}
+
+sortJson('fr.json', 'fr.json')
+sortJson('en.json', 'en.json')
 
 
