abbf6a6e7294aa0d8355c40ddf8362630af3c1fd
BasilRebresh <rebresh58gmail.com>
2023-03-13 17:35:30 +0200

    helping console logs

--- src/components/Scanner/index.js
+++ src/components/Scanner/index.js
@@ -108,7 +108,7 @@ export default function Scanner({
   })
 
   // eslint-disable-next-line no-console
-  console.log('trying to fix 16:36')
+  console.log('trying to fix 16:36 Scanner')
 
   return (
     <Wrapper height={W}>
