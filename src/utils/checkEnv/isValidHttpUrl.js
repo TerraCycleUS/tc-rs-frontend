a205d70b8a6e9060bdf610a0e4fd6b6b996a5905
BasilRebresh <rebresh58gmail.com>
2023-04-05 12:29:58 +0300

    fixed stage checking

--- src/utils/checkEnv/isValidHttpUrl.js
+++ src/utils/checkEnv/isValidHttpUrl.js
@@ -1,14 +1,3 @@
 export function isValidHttpUrl(string) {
-  try {
-    const url = new URL(string)
-    // console.log('url', url)
-    // console.log('url', url.protocol)
-    // console.log('url.protocol === \'http:\' || url.protocol === \'https:\'', url.protocol === 'http:' || url.protocol === 'https:')
-
-    // return url.protocol === 'http:' || url.protocol === 'https:'
-    // return url.protocol === 'https:'
-    return url.protocol === 'https:'
-  } catch (err) {
-    return false
-  }
+  return string.includes('stage')
 }
