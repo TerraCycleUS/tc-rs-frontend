5307e36e0579b130115695c1494bdaa174cee3c3
BasilRebresh <rebresh58gmail.com>
2023-04-12 12:52:10 +0300

    added loader for admin dashboard

--- src/components/ForAdminPanel/Dashboard/index.js
+++ src/components/ForAdminPanel/Dashboard/index.js
@@ -2,23 +2,20 @@ import React, { useEffect, useState } from 'react'
 import { CRow, CCol, CWidgetStatsA } from '@coreui/react'
 import '@coreui/coreui/scss/coreui-utilities.scss'
 import './_dashboard.scss'
+import CircularProgress from '@mui/material/CircularProgress'
 import http from '../../../utils/http'
 import useApiCall from '../../../utils/useApiCall'
 import 'react-day-picker/dist/style.css'
 
 export default function Dashboard() {
-  const [userCounter, setUserCounter] = useState(0)
-  const [productCounter, setProductCounter] = useState(0)
-  const [unlockedCouponCounter, setUnlockedCouponCounter] = useState(0)
+  const [dashboardInfo, setDashboardInfo] = useState()
   const getDashboardDataApiCall = useApiCall()
 
   function getData() {
     return getDashboardDataApiCall(
       () => http.get('/api/admin/dashboard-info'),
       (response) => {
-        setUserCounter(response.data?.userCounter)
-        setProductCounter(response.data?.productCounter)
-        setUnlockedCouponCounter(response.data?.unlockedCouponCounter)
+        setDashboardInfo(response.data)
       },
       null,
       null,
@@ -32,40 +29,58 @@ export default function Dashboard() {
 
   useEffect(() => {
     document.addEventListener('refresh', getData)
-
     return () => {
       document.removeEventListener('refresh', getData)
     }
   }, [])
 
   return (
-    <CRow className="dashBoardContainer">
-      <CCol sm={4}>
-        <CWidgetStatsA
-          className="mb-4"
-          color="primary"
-          value={<div className="fs-4 fw-semibold">{userCounter}</div>}
-          title="Registered users"
-        />
-      </CCol>
-      <CCol sm={4}>
-        <CWidgetStatsA
-          className="mb-4"
-          color="info"
-          value={
-            <div className="fs-4 fw-semibold">{unlockedCouponCounter}</div>
-          }
-          title="Coupons redeemed"
-        />
-      </CCol>
-      <CCol sm={4}>
-        <CWidgetStatsA
-          className="mb-4"
-          color="warning"
-          value={<div className="fs-4 fw-semibold">{productCounter}</div>}
-          title="Products scanned"
-        />
-      </CCol>
-    </CRow>
+    // eslint-disable-next-line react/jsx-no-useless-fragment
+    <>
+      {dashboardInfo ? (
+        <CRow className="dashBoardContainer">
+          <CCol sm={4}>
+            <CWidgetStatsA
+              className="mb-4"
+              color="primary"
+              value={
+                <div className="fs-4 fw-semibold">
+                  {dashboardInfo.userCounter}
+                </div>
+              }
+              title="Registered users"
+            />
+          </CCol>
+          <CCol sm={4}>
+            <CWidgetStatsA
+              className="mb-4"
+              color="info"
+              value={
+                <div className="fs-4 fw-semibold">
+                  {dashboardInfo.unlockedCouponCounter}
+                </div>
+              }
+              title="Coupons redeemed"
+            />
+          </CCol>
+          <CCol sm={4}>
+            <CWidgetStatsA
+              className="mb-4"
+              color="warning"
+              value={
+                <div className="fs-4 fw-semibold">
+                  {dashboardInfo.productCounter}
+                </div>
+              }
+              title="Products scanned"
+            />
+          </CCol>
+        </CRow>
+      ) : (
+        <div className="loaderWrapper">
+          <CircularProgress />
+        </div>
+      )}
+    </>
   )
 }
