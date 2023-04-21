3b69425a4b50f83c233594bfcc447db1d85f9c27
BasilRebresh <rebresh58gmail.com>
2023-04-21 11:01:30 +0300

    error message fix on PictureExport

--- src/components/ForAdminPanel/PictureExport/index.js
+++ src/components/ForAdminPanel/PictureExport/index.js
@@ -38,13 +38,9 @@ export default function PictureExport() {
         ),
       null,
       (error) => {
-        // eslint-disable-next-line no-console
-        console.log('error', error)
-        // eslint-disable-next-line no-console
-        console.log('error?.response?.data', error?.response?.data)
         if (error?.response?.data?.errorCode === 'prevTaskInProgress')
           notify('Previous export is still generating')
-        notify(error?.response?.data?.message || 'Error')
+        else notify(error?.response?.data?.message || 'Error')
       },
       null,
       { retry: false, message: false },
