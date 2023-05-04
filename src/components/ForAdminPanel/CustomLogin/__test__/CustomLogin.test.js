b8550ac6d6347b70211b6ed1e35fc66c079c2304
BasilRebresh <rebresh58gmail.com>
2023-05-04 16:36:44 +0300

    CustomLogin test

---
+++ src/components/ForAdminPanel/CustomLogin/__test__/CustomLogin.test.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { render } from '@testing-library/react'
+import { AdminContext } from 'react-admin'
+import CustomLogin from '../index'
+import '@testing-library/jest-dom'
+
+jest.mock('../../../../utils/http')
+jest.mock('../../../../utils/useApiCall', () => () => jest.fn(() => {}))
+
+describe('CustomLogin', () => {
+  test('it renders CustomLogin component', async () => {
+    render(
+      <AdminContext>
+        <CustomLogin />
+      </AdminContext>,
+    )
+  })
+})
