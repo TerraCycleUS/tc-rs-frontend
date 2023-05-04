a86e3c6c3d953daaaa7ae57f4c73470f765f801b
BasilRebresh <rebresh58gmail.com>
2023-05-04 16:09:10 +0300

    CouponEdit test

---
+++ src/components/ForAdminPanel/Coupon/CouponEdit/__test__/CouponEdit.test.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { render } from '@testing-library/react'
+import { AdminContext, Resource } from 'react-admin'
+import CouponEdit from '../index'
+import '@testing-library/jest-dom'
+import TestEnvironment from '../../../../ForTestWriting/TestEnvironment'
+import store from '../../../../../store'
+
+jest.mock('../../../../../utils/http')
+jest.mock('../../../../../utils/useApiCall', () => () => jest.fn(() => {}))
+
+describe('CouponEdit', () => {
+  test('it renders CouponEdit component', async () => {
+    render(
+      <TestEnvironment store={store} initialEntries={['/coupon/1']}>
+        <AdminContext>
+          <Resource name="coupon" edit={CouponEdit} />
+        </AdminContext>
+      </TestEnvironment>,
+    )
+  })
+})
