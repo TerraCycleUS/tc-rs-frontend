7ca8a0dc22aa5faba6b60099ec4489654041a1c9
BasilRebresh <rebresh58gmail.com>
2023-05-10 14:57:23 +0300

    Message, Backdrop, BackdropMessage small refactor. Getting rid of styled-component

--- src/components/Message/BackdropMessage.js
+++ src/components/Message/BackdropMessage.js
@@ -2,7 +2,7 @@ import classNames from 'classnames'
 import React from 'react'
 
 import Message from '.'
-import Backdrop from '../Backdrop'
+import classes from './Message.module.scss'
 
 export default function BackdropMessage({
   type = 'success',
@@ -12,10 +12,11 @@ export default function BackdropMessage({
   className,
 }) {
   return (
-    <Backdrop
+    // eslint-disable-next-line jsx-a11y/click-events-have-key-events,jsx-a11y/no-static-element-interactions
+    <div
       onClick={onClose}
       className={classNames(
-        'container',
+        classes.backDrop,
         'd-flex',
         'justify-content-center',
         'align-items-center',
@@ -30,7 +31,7 @@ export default function BackdropMessage({
       >
         {children}
       </Message>
-    </Backdrop>
+    </div>
   )
 }
 
--- src/components/Message/index.js
+++ src/components/Message/index.js
@@ -1,11 +1,8 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import styled from 'styled-components'
 import classNames from 'classnames'
-
 import { ReactComponent as Close } from '../../assets/icons/close.svg'
-import error from '../../assets/icons/error.svg'
-import success from '../../assets/icons/success.svg'
+import classes from './Message.module.scss'
 
 export default function Message({
   type = 'success',
@@ -21,16 +18,16 @@ export default function Message({
   }
 
   return (
-    <Wrapper
-      type={type}
-      className={classNames('message', type, className)}
+    // eslint-disable-next-line jsx-a11y/click-events-have-key-events,jsx-a11y/no-static-element-interactions
+    <div
+      className={classNames(classes.messageWrapper, 'message', type, className)}
       onClick={(e) => e.stopPropagation()}
     >
       {content}
       <button onClick={onClose} type="button" className="close">
         {onClose ? <Close /> : null}
       </button>
-    </Wrapper>
+    </div>
   )
 }
 
@@ -41,47 +38,3 @@ Message.propTypes = {
   onClose: PropTypes.func,
   className: PropTypes.string,
 }
-
-const Wrapper = styled.div`
-  padding: 20px 16px;
-  background-color: #fff;
-  box-shadow: 0px 3px 10px rgba(48, 48, 48, 0.1);
-  border-radius: 20px;
-  display: flex;
-  justify-content: space-between;
-
-  &::before {
-    content: '';
-    display: block;
-    width: 24px;
-    height: 24px;
-    border-radius: 50%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    background-position: center;
-    background-repeat: no-repeat;
-    background-size: 60%;
-    background-color: ${({ theme, type }) => theme[type] || theme.success};
-    flex-shrink: 0;
-  }
-
-  &.error::before {
-    background-image: url(${error});
-  }
-
-  &.success::before {
-    background-image: url(${success});
-  }
-
-  .message-content {
-    font-weight: 400;
-    font-size: 14px;
-    line-height: 25px;
-  }
-
-  .close {
-    width: 24px;
-    height: 24px;
-  }
-`
