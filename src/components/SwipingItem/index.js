ed964ecce235e70196e14322215aac2be993e3a7
BasilRebresh <rebresh58gmail.com>
2023-05-16 15:07:14 +0300

    SwipingItem minor refactor, removing styled-component

--- src/components/SwipingItem/index.js
+++ src/components/SwipingItem/index.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import { useSwipeable } from 'react-swipeable'
-import styled from 'styled-components'
 import PropTypes from 'prop-types'
+import classes from './SwipingItem.module.scss'
 
 export default function SwipingItem({
   children,
@@ -44,10 +44,11 @@ export default function SwipingItem({
   }
 
   return (
-    <SwipingContainer {...handlers} style={{ height }}>
-      <SwipingActionContainer style={{ height }}>
+    <div className={classes.swipingContainer} {...handlers} style={{ height }}>
+      <div className={classes.swipingActionContainer} style={{ height }}>
         {actionButtons.map((action) => (
-          <ActionButton
+          <button
+            className={classes.actionButton}
             type="button"
             key={action.key}
             onClick={() => handleActionClicked(action.onClick)}
@@ -58,10 +59,11 @@ export default function SwipingItem({
             role={action.role || 'button'}
           >
             {action.content}
-          </ActionButton>
+          </button>
         ))}
-      </SwipingActionContainer>
-      <SwipeableContentContainer
+      </div>
+      <div
+        className={classes.swipeableContentContainer}
         style={{
           height,
           transform: `translateX(${
@@ -72,42 +74,11 @@ export default function SwipingItem({
         }}
       >
         {children}
-      </SwipeableContentContainer>
-    </SwipingContainer>
+      </div>
+    </div>
   )
 }
 
-export const SwipingContainer = styled.div`
-  width: 100%;
-  position: relative;
-  margin-bottom: 10px;
-`
-
-export const SwipingActionContainer = styled.div`
-  width: 100%;
-  position: absolute;
-  top: 0;
-  right: 0;
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: flex-end;
-  background-color: ${({ theme }) => theme.delete};
-  border-radius: 15px;
-  border-width: 1px 1px 1px 1px;
-  border-style: solid;
-  border-color: ${({ theme }) => theme.terraGrey};
-`
-
-export const SwipeableContentContainer = styled.div`
-  width: 100%;
-  transition: transform 300ms linear;
-  -webkit-transition: -webkit-transform 300ms linear;
-  -webkit-transition: transform 300ms linear;
-`
-export const ActionButton = styled.button`
-  background-color: transparent;
-`
 SwipingItem.propTypes = {
   children: PropTypes.node,
   actionButtons: PropTypes.array,
