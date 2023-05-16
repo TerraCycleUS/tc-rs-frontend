5559aeee93191df5223feb587a34d11e5d9c1b3a
BasilRebresh <rebresh58gmail.com>
2023-05-16 15:41:12 +0300

    RetailerMenu test

--- src/components/RetailerMenu/__test__/RetailerMenu.test.js
+++ src/components/RetailerMenu/__test__/RetailerMenu.test.js
@@ -1,20 +1,32 @@
 import React from 'react'
 import { render } from '@testing-library/react'
 import RetailerMenu from '../index'
-import store from '../../../store'
-import { setUser } from '../../../actions/user'
-import TestEnvironment from '../../ForTestWriting/TestEnvironment'
-
-jest.mock('../../../utils/http')
-jest.mock('../../../utils/useApiCall', () => () => jest.fn(() => {}))
 
 describe('RetailerMenu ', () => {
   test('it renders Retailer menu', async () => {
-    store.dispatch(setUser({ user: 'mock' }))
+    render(<RetailerMenu />)
+  })
+
+  test('it renders Retailer menu while using index prop', async () => {
     render(
-      <TestEnvironment store={store}>
-        <RetailerMenu />
-      </TestEnvironment>,
+      <RetailerMenu
+        retailers={[{ id: 2, name: 'Carrefour', index: 2 }]}
+        setActiveRetailer={() => {}}
+        activeRetailer={1}
+        className="mock"
+        useIndex
+      />,
+    )
+  })
+
+  test('it renders Retailer menu with no index prop', async () => {
+    render(
+      <RetailerMenu
+        retailers={[{ id: 2, name: 'Carrefour', index: 2 }]}
+        setActiveRetailer={() => {}}
+        activeRetailer={1}
+        className="mock"
+      />,
     )
   })
 })
