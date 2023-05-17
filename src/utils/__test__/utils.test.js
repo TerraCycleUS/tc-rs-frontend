713c26c1957b11af25672a3cae7766dfe4e349a0
BasilRebresh <rebresh58gmail.com>
2023-05-17 15:00:19 +0300

    utils test for getProgressPercentage, getMobileOperatingSystem and extractErrorMessage

--- src/utils/__test__/utils.test.js
+++ src/utils/__test__/utils.test.js
@@ -1,5 +1,7 @@
 import extractErrorMessage from '../extractErrorMessage'
 import '@testing-library/jest-dom'
+import getProgressPercentage from '../getProgressPercentage'
+import getMobileOperatingSystem from '../getMobileOperatingSystem'
 
 describe('utils testing', () => {
   test('extractErrorMessage will find error in response to display', async () => {
@@ -18,5 +20,37 @@ describe('utils testing', () => {
     expect(extractErrorMessage(input2)).toBe('One error text\nOne error text')
     expect(extractErrorMessage(input3)).toBe('Error message')
     expect(extractErrorMessage(input4)).toBe('Error message')
+  })
+
+  test('getProgressPercentage count percentage of progress for unlocking', async () => {
+    expect(getProgressPercentage(5, 10)).toBe('50%')
+    expect(getProgressPercentage(10, 5)).toBe('100%')
+    expect(getProgressPercentage(7, 13)).toBe('53.84615384615385%')
+    expect(getProgressPercentage(13, 7)).toBe('100%')
+  })
+
+  test('getMobileOperatingSystem correctly returns link for android and ios', async () => {
+    const androidNavigator = {
+      userAgent: 'android',
+    }
+    const iphoneNavigator = {
+      userAgent: 'iPhone',
+    }
+
+    Object.defineProperty(global, 'navigator', {
+      value: androidNavigator,
+      writable: true,
+    })
+    expect(getMobileOperatingSystem(5, 10)).toBe(
+      process.env.REACT_APP_LINK_TO_GOOGLEPLAY,
+    )
+
+    Object.defineProperty(global, 'navigator', {
+      value: iphoneNavigator,
+      writable: true,
+    })
+    expect(getMobileOperatingSystem(5, 10)).toBe(
+      process.env.REACT_APP_LINK_TO_APPSTORE,
+    )
   })
 })
