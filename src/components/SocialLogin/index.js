aeed6aae3967648b2925d3e2df7846f65d3779bf
BasilRebresh <rebresh58gmail.com>
2023-05-24 10:54:06 +0300

    SocialLogin styled-component removed

--- src/components/SocialLogin/index.js
+++ src/components/SocialLogin/index.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import styled from 'styled-components'
 import { FormattedMessage } from 'react-intl'
 
 import PropTypes from 'prop-types'
@@ -8,45 +7,29 @@ import { ReactComponent as Facebook } from '../../assets/icons/facebook.svg'
 import LogInButton from '../Button/LoginButton'
 import Text from '../Text'
 import { fbLoginUrl, googleLoginUrl } from '../../utils/socialLoginUrl'
+import classes from './SocialLogin.module.scss'
 
 export default function SocialLogin({ language }) {
   return (
-    <Wrapper>
+    <div className={classes.wrapper}>
       <Text className="text-center">
         <FormattedMessage
           id="signUp:SocialLogin"
           defaultMessage="or use your social account for log in:"
         />
       </Text>
-      <div className="buttons-row">
-        <LogInButton className="google" href={googleLoginUrl(language)}>
+      <div className={classes.buttonsRow}>
+        <LogInButton className={classes.google} href={googleLoginUrl(language)}>
           <GooglePlus />
         </LogInButton>
-        <LogInButton className="facebook" href={fbLoginUrl(language)}>
+        <LogInButton className={classes.facebook} href={fbLoginUrl(language)}>
           <Facebook />
         </LogInButton>
       </div>
-    </Wrapper>
+    </div>
   )
 }
 
 SocialLogin.propTypes = {
   language: PropTypes.string,
 }
-
-const Wrapper = styled.div`
-  .buttons-row {
-    margin-top: 10px;
-    display: flex;
-    justify-content: center;
-
-    .google {
-      background-color: #f06552;
-      margin-right: 30px;
-    }
-
-    .facebook {
-      background-color: #4267b2;
-    }
-  }
-`
