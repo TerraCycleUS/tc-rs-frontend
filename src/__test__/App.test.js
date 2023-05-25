160bbdf4b7db08630aeef9baff5f7fca9d1f4d38
BasilRebresh <rebresh58gmail.com>
2023-05-25 17:32:38 +0300

    test for App.js, removing index.js from coverage

---
+++ src/__test__/App.test.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { render } from '@testing-library/react'
+import TestEnvironment from '../components/ForTestWriting/TestEnvironment'
+// import store from '../../../store'
+import App from '../App'
+
+jest.mock('../utils/http')
+jest.mock('../utils/useApiCall', () => () => jest.fn(() => {}))
+
+describe('App ', () => {
+  test('it renders App', async () => {
+    render(
+      <TestEnvironment>
+        <App />
+      </TestEnvironment>,
+    )
+  })
+})
