302d5101235c57592879ce32226b91f938e8a6a8
BasilRebresh <rebresh58gmail.com>
2023-05-30 12:45:34 +0300

    several simple rendering tests

---
+++ src/components/PopUps/CashTillBarcode/__test__/CashTillBarcode.test.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { render } from '@testing-library/react'
+import CashTillBarcode from '../index'
+import TestEnvironment from '../../../ForTestWriting/TestEnvironment'
+
+describe('CashTillBarcode', () => {
+  test('it renders CashTillBarcode', async () => {
+    render(
+      <TestEnvironment>
+        <CashTillBarcode />
+      </TestEnvironment>,
+    )
+  })
+})
---
+++ src/components/PopUps/ConfirmDrop/__test__/ConfirmDrop.test.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { render } from '@testing-library/react'
+import ConfirmDrop from '../index'
+import TestEnvironment from '../../../ForTestWriting/TestEnvironment'
+
+describe('ConfirmDrop', () => {
+  test('it renders ConfirmDrop', async () => {
+    render(
+      <TestEnvironment>
+        <ConfirmDrop setShowPop={() => {}} />
+      </TestEnvironment>,
+    )
+  })
+})
