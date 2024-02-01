32924a01eeae2427e256d32f369af0d8642e13a7
Drolfothesgnir <kyryl.yeletsky@gmail.com>
2024-02-01 20:53:53 +0200

    added retailer assignment funtionality; disabled monoprix in profile retailer edit

--- src/pages/RetailerListPage/index.js
+++ src/pages/RetailerListPage/index.js
@@ -8,6 +8,7 @@ import classes from "./RetailerListPage.module.scss";
 import useApiCall from "../../utils/useApiCall";
 import http from "../../utils/http";
 import RetailerList from "../../components/RetailerList";
+import { MONOPRIX_ID } from "../../utils/const";
 
 export default function RetailerListPage() {
   const [retailers, setRetailers] = useState([]);
@@ -17,6 +18,9 @@ export default function RetailerListPage() {
     getMyRetailersApiCall(
       () => http.get("/api/retailer/my-retailers"),
       (response) => {
+        response.data.forEach((item) => {
+          item.disabled = item.id === MONOPRIX_ID;
+        });
         setRetailers(response.data);
       },
       null,
