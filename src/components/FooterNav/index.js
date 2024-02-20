2d5ad1b6244b1aa7df8caeb7c1b257b250d9b57a
Drolfothesgnir <kyryl.yeletsky@gmail.com>
2024-02-20 14:32:26 +0200

    added retailer sorting on coupons page instead of retailer list

--- src/components/FooterNav/index.js
+++ src/components/FooterNav/index.js
@@ -40,7 +40,7 @@ const links = [
     end: true,
   },
   {
-    to: "/rewards-wallet",
+    to: "/rewards-wallet/rewards",
     icon: <Coupon className={classes.navIcon} />,
     label: {
       id: "footer:Rewards",
