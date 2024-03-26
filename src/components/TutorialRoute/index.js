bde66a6c484aac520e0644921509804ab48ce522
Drolfothesgnir <kyryl.yeletsky@gmail.com>
2024-03-26 13:19:39 +0200

    fixed intro screen button width; intro screen removed from tutorial

--- src/components/TutorialRoute/index.js
+++ src/components/TutorialRoute/index.js
@@ -6,7 +6,7 @@ import PropTypes from "prop-types";
 export default function TutorialRoute({ children }) {
   const seenTutorial = useSelector((state) => state.seenTutorial);
 
-  if (!seenTutorial) return <Navigate to="/profile/tutorial" replace />;
+  if (!seenTutorial) return <Navigate to="/profile/introduction" replace />;
 
   return children;
 }
