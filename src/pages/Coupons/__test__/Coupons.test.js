f8ad836756c24de912cd5dc4d21d97cd7e0c25d8
Drolfothesgnir <kyryl.yeletsky@gmail.com>
2024-06-12 18:39:31 +0300

    added calculateCouponWeight function test

--- src/pages/Coupons/__test__/Coupons.test.js
+++ src/pages/Coupons/__test__/Coupons.test.js
@@ -4,6 +4,7 @@ import Coupons from "../index";
 import store from "../../../store";
 import { setUser } from "../../../actions/user";
 import TestEnvironment from "../../../components/ForTestWriting/TestEnvironment";
+import { calculateCouponWeight } from "../couponsUtils";
 
 jest.mock("../../../utils/http");
 jest.mock("../../../utils/useApiCall", () => () => jest.fn(() => {}));
@@ -16,5 +17,26 @@ describe("Coupons", () => {
         <Coupons />
       </TestEnvironment>
     );
+  });
+
+  test("check calculateCouponWeight function", () => {
+    const mockCouponReady1 = {
+      requiredAmount: 10,
+      discount: 15,
+      retailerId: 1,
+      availableAmount: 10,
+    };
+
+    const mockCouponReady2 = {
+      requiredAmount: 10,
+      discount: 25,
+      retailerId: 2,
+      availableAmount: 10,
+    };
+
+    const weight1 = calculateCouponWeight(mockCouponReady1);
+    const weight2 = calculateCouponWeight(mockCouponReady2);
+
+    expect(weight1).toBeGreaterThan(weight2);
   });
 });
