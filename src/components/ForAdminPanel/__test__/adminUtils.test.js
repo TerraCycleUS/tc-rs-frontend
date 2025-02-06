2fc21f2022f5040c18d7e24d780028dfadc72029
Bogdan Serhienko <bodis90@gmail.com>
2025-02-06 23:54:26 +0200

    Fix render category titile and retailers

--- src/components/ForAdminPanel/__test__/adminUtils.test.js
+++ src/components/ForAdminPanel/__test__/adminUtils.test.js
@@ -1,26 +1,7 @@
-import { findRetailer, onError, formatForApi } from "../adminUtils";
+import { onError, formatForApi } from "../adminUtils";
 import "@testing-library/jest-dom";
 
 describe("paginationSlice", () => {
-  test("findRetailer returns name by id", async () => {
-    const mockRetailers = [
-      {
-        id: 2,
-        name: "Carrefour",
-        description:
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit,sed do eiusmod tempor incididunt ut labore et dolore.uis nostrud exercitationullamco laboris nisi ut aliquip ex ea commodo consequat",
-        backgroundImage: "http:ca3a43cd02f.jpg",
-        logo: "http://e577fe1f42f.jpg",
-        smallLogo: "http://12-c2b5ff810076.png",
-        userLoyaltyCode: null,
-        userLoyaltyPassCode: "1030750000000000000",
-      },
-    ];
-
-    const output = findRetailer(2, mockRetailers);
-    expect(output).toBe("Carrefour");
-  });
-
   test("onError finds error message in body", async () => {
     const mockError1 = { error: { body: { errors: "OOps" } } };
     const mockError2 = { error: { body: { message: "Something went wrong" } } };
